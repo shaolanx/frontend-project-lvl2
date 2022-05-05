ddc9a4d30162a205ee3a1997b51dd8be10561252
Иван <daggerx@mail.ru>
2022-05-05 12:06:07 +0300

    1

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,17 +2,3 @@
 
 console.log('Hi')
 
-
-import { Command } from 'commander';
-
-
-const program = new Command();
-
-program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1', '-V, --version', 'output the version number')
-  .option('-f, --format <type>', 'output format')
-
-
-export default program.parse();
