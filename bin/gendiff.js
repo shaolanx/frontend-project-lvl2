eede607ea706876aea9f635605e1d065a4b30f2e
Иван Марченко <daggerx@mail.ru>
2022-05-05 20:30:34 +0300

    it's alive!!!!

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,4 +1,12 @@
 #!/usr/bin/env node
 
-console.log('Hi')
+import { Command } from 'commander';
 
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-V, --version', 'output the version number')
+
+export default program.parse();
