7c935fd4263da60c8cbb936680ba766c97416008
shaolanx <daggerx@mail.ru>
2022-05-25 16:58:30 +0300

    step 6 fix

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,11 +8,11 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1', '-V, --version', 'output the version number')
-  .option('-f, --format <type>', 'output format')
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
   });
 
 export default program.parse();
