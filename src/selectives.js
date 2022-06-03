6f6efddb46a398a78b5f2e4690bc062e8848c4f5
shaolanx <daggerx@mail.ru>
2022-06-03 15:13:27 +0300

    lint fix

--- src/selectives.js
+++ src/selectives.js
@@ -7,8 +7,7 @@ const selectOperator = (obj) => {
     return '-';
   }
   return ' ';
-  };
-  
+};
 const prepareData = (obj1, obj2, key) => {
   const objectComparison = obj1[key] === obj2[key];
   if (!_.has(obj1, key)) {
@@ -17,13 +16,12 @@ const prepareData = (obj1, obj2, key) => {
     return { key, value: obj1[key], status: 'deleted' };
   } if (!objectComparison) {
     return [{
-      key, value: obj1[key], newValue: obj2[key], status: 'changedAndParentIsObj1',
-      }, {
-      key, oldValue: obj1[key], value: obj2[key], status: 'changedAndParentIsObj2',
-      }];
+    key, value: obj1[key], newValue: obj2[key], status: 'changedAndParentIsObj1',
+    }, {
+    key, oldValue: obj1[key], value: obj2[key], status: 'changedAndParentIsObj2',
+    }];
   }
     return { key, value: obj1[key], status: 'unchanged' };
 };
 
-  export { selectOperator, prepareData };
-  
+export { selectOperator, prepareData };
