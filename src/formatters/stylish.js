277c94ee53f5003463160c5e23ae768fcd6ccf5c
shaolanx <daggerx@mail.ru>
2022-06-10 15:14:17 +0300

    ашч ыендшыр 2

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,32 +1,35 @@
 import _ from 'lodash';
 
+const currentIndent = depthes => ' '.repeat(depthes * 2);
+
+const stringify = (value, depth) => {
+  if (!(value instanceof Object)) {
+    return value;
+  }
+  const openingSpaces = currentIndent(depth + 2);
+  const closingSpaces = currentIndent(depth);
+  if (value.status === 'has children') {
+    return `{\n${openingSpaces} ${stringify(Object.values(value), 4)}\n${closingSpaces}}`;
+  }
+  return `{\n${openingSpaces} ${Object.values(value)}\n${closingSpaces}  }`;
+};
+
 const iter = (tree, depth = 1) => {
-  const currentIndent = depthes => ' '.repeat(depthes * 2);
-
-  const stringify = (value, depth) => {
-    if (!(value instanceof Object)) {
-     return value;
-    }
-    const openingSpaces = currentIndent(depth + 2);
-    const closingSpaces = currentIndent(depth);
-    return `{\n${openingSpaces}${Object.keys(value)}: ${Object.values(value)}\n${closingSpaces}}`;
-  };
-
   const result = tree.map((node) => {
-    switch (node.type) {
+    switch (node.status) {
       case 'deleted':
-        return `${currentIndent(depth)}- ${stringify(node.value, depth)}`;
+        return `${currentIndent(depth)}- ${node.key}: ${stringify(node.value, depth)}`;
       case 'added':
-        return `${currentIndent(depth)}+ ${stringify(node.value, depth)}`;
+        return `${currentIndent(depth)}+ ${node.key}: ${stringify(node.value, depth)}`;
       case 'unchanged':
-        return `${currentIndent(depth)}  ${stringify(node.value, depth)}`;
+        return `${currentIndent(depth)}   ${node.key}: ${stringify(node.value, depth)}`;
       case 'updated':
         return [
-          `${currentIndent(depth)}- ${stringify(node.value, depth)}`,
-          `${currentIndent(depth)}+ ${stringify(node.newValue, depth)}`,
+          `${currentIndent(depth)}- ${node.key}: ${stringify(node.value, depth)}`,
+          `${currentIndent(depth)}+ ${node.key}: ${stringify(node.newValue, depth)}`,
         ];
       case 'has children':
-        return `${currentIndent(depth)}  {\n${iter(node.children, depth + 2)}\n${currentIndent(depth)}}`;
+        return `${currentIndent(depth)} ${node.key}: {\n${iter(node.children, depth + 2)}\n${currentIndent(depth)}}`;
       default:
         throw new Error('missing selector');
     }
